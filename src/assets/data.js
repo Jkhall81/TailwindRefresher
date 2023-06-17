37e1f92998174de36495890f56affc935d8f26fe
Jkhall81 <jason.kei.hall@gmail.com>
2023-06-17 07:31:47 -0700

    put my props data in a seperate js file, imported it in and passed it to the rocketcard components using the map method.  been busy with python, flexing the old javascript muscles a little bit.

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+const rocketData = [
+  {
+    imageSrc: "https://picsum.photos/200/300?random=1",
+    headingText: "Explorer",
+    paragraphTextOne: "$",
+    paragraphTextTwo: "Affordable Exploration",
+  },
+  {
+    imageSrc: "https://picsum.photos/200/300?random=2",
+    headingText: "Adventurer",
+    paragraphTextOne: "$$",
+    paragraphTextTwo: "Best Selling Rocket",
+  },
+  {
+    imageSrc: "https://picsum.photos/200/300?random=3",
+    headingText: "Infinity",
+    paragraphTextOne: "$$$",
+    paragraphTextTwo: "Luxury Starship",
+  },
+];
+
+export default rocketData;
--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,32 +1,24 @@
 import React from "react";
 import RocketCard from "./RocketCard";
+import rocketData from "../assets/data.js";
 
-const rockets = () => {
+const Rockets = () => {
   return (
     <>
-      <section id="rockets" className="p-6 my-12">
+      <section id="rockets" className="p-6 my-12 scroll-mt-20">
         <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
           Our Rockets
         </h2>
         <ul className="list-none mx-auto my-12 flex flex-col sm:flex-row items-center gap-8">
-          <RocketCard
-            imageSrc="https://picsum.photos/200/300?random=1"
-            headingText="Explorer"
-            paragraphTextOne="$"
-            paragraphTextTwo="Affordable Exploration"
-          />
-          <RocketCard
-            imageSrc="https://picsum.photos/200/300?random=2"
-            headingText="Adventurer"
-            paragraphTextOne="$$"
-            paragraphTextTwo="Best Selling Rocket"
-          />
-          <RocketCard
-            imageSrc="https://picsum.photos/200/300?random=3"
-            headingText="Infinity"
-            paragraphTextOne="$$$"
-            paragraphTextTwo="Luxury Starship"
-          />
+          {rocketData.map((rocket, index) => (
+            <RocketCard
+              key={index}
+              imageSrc={rocket.imageSrc}
+              headingText={rocket.headingText}
+              paragraphTextOne={rocket.paragraphTextOne}
+              paragraphTextTwo={rocket.paragraphTextTwo}
+            />
+          ))}
         </ul>
       </section>
       <hr className="w-1/2 mx-auto bg-black dark:bg-white" />
@@ -34,4 +26,4 @@ const rockets = () => {
   );
 };
 
-export default rockets;
+export default Rockets;
